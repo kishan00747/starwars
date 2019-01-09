2b9d52f1e5c8f291c6d9ed654668f2a78c3bf3a8
kishan00747 <kishan00747@gmail.com>
2019-01-09 13:54:05 +0530

    Updates

---
+++ precache-manifest.8fb88ff893d48090536ad564c26309c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65895581d22e120e4c7e",
+    "url": "/static/js/main.65895581.chunk.js"
+  },
+  {
+    "revision": "14b93e6d231599d9a353",
+    "url": "/static/js/1.14b93e6d.chunk.js"
+  },
+  {
+    "revision": "65895581d22e120e4c7e",
+    "url": "/static/css/main.fa01e057.chunk.css"
+  },
+  {
+    "revision": "14b93e6d231599d9a353",
+    "url": "/static/css/1.8e1193f9.chunk.css"
+  },
+  {
+    "revision": "170741a538fa2ea1f5516e08ca7983c8",
+    "url": "/index.html"
+  }
+];
